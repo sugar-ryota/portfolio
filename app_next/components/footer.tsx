70866c3f99873973bdb8098fd834c260810dd87f
sugar-ryota <s1811345@s.tsukuba.ac.jp>
2022-05-17 01:46:39 +0900

    install next.js in docker

---
+++ app_next/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(props: any) {
+  return (
+    <div className="text-center h6 my-4">
+      <div>{props.footer}</div>
+    </div>
+  )
+}
---
+++ app_next/pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from '../components/layout'
+import MyImage from '../components/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Layout header="Next.js" title="Top page.">
+        <div className="alert alert-primary text-center">
+          <h5 className="mb-4">Welcome to next.js!</h5>
+          <MyImage fname="portfolio.png" size="300" />
+        </div>
+      </Layout>
+    </div>
+  )
+}
